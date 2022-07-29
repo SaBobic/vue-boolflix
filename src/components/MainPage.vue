dd5cd3a7cac51ed491e8cb7c8be2757108b58cec
SaBobic <fabrizioettori@gmail.com>
2022-07-29 15:52:57 +0200

    added BaseCard component

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -4,21 +4,7 @@
             <h2>Film</h2>
             <div class="row">
                 <div v-for="film in filmsList" :key="film.id" class="col">
-                    <div class="_card">
-                        <h4>{{ film.title }}</h4>
-                        <div>{{ film.original_title }}</div>
-                        <div v-if="film.poster_path">
-                            <img :src="`${baseImgUri}/w342${film.poster_path}`" :alt="film.title">
-                        </div>
-                        <div v-if="film.original_language === 'it' || film.original_language === 'en'">
-                            <img :src="require(`..//assets/img/${film.original_language}.png`)" alt="">
-                        </div>
-                        <div v-else>{{ film.original_language }}</div>
-                        <div v-if="film.vote_average">
-                            <i v-for="index in getCorrectVote(film.vote_average)" class="fa-solid fa-star"
-                                :key="index"></i>
-                        </div>
-                    </div>
+                    <BaseCard :production="film" />
                 </div>
             </div>
         </section>
@@ -27,21 +13,7 @@
             <h2>Serie TV</h2>
             <div class="row">
                 <div v-for="show in showsList" :key="show.id" class="col">
-                    <div class="_card">
-                        <h4>{{ show.name }}</h4>
-                        <div>{{ show.original_name }}</div>
-                        <div v-if="show.poster_path">
-                            <img :src="`${baseImgUri}/w342${show.poster_path}`" :alt="show.name">
-                        </div>
-                        <div v-if="show.original_language === 'it' || show.original_language === 'en'">
-                            <img :src="require(`../assets/img/${show.original_language}.png`)" alt="">
-                        </div>
-                        <div v-else>{{ show.original_language }}</div>
-                        <div v-if="show.vote_average">
-                            <i v-for="index in getCorrectVote(show.vote_average)" class="fa-solid fa-star"
-                                :key="index"></i>
-                        </div>
-                    </div>
+                    <BaseCard :production="show" />
                 </div>
             </div>
         </section>
@@ -49,12 +21,13 @@
 </template>
 
 <script>
+import BaseCard from './BaseCard.vue';
 export default {
-    name: 'MainPage',
+    name: "MainPage",
     data() {
         return {
-            baseImgUri: 'https://image.tmdb.org/t/p',
-        }
+            baseImgUri: "https://image.tmdb.org/t/p",
+        };
     },
     props: {
         filmsList: Array,
@@ -64,7 +37,8 @@ export default {
         getCorrectVote(number) {
             return Math.ceil(number * 0.5);
         },
-    }
+    },
+    components: { BaseCard }
 }
 </script>
 
